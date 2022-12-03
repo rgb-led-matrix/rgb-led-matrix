56dc8271d066eaad8bec9479f8ad960d9616da6c
David Thacher <davidethacher@gmail.com>
2022-12-03 00:37:01 -0500

    Updates

--- rgb-led-matrix/lib/src/port/rpi/main.cpp
+++ rgb-led-matrix/lib/src/port/rpi/main.cpp
@@ -1,5 +1,19 @@
+#include "canvas.h"
+#include "led-matrix.h"
+using namespace rgb_matrix;
 
 int main(int argc, char **argv) {
+    Options options;
+    RGBMatrix *matrix = RGBMatrix::CreateFromOptions(options);
+    Canvas *canvas0 = matrix->CreateCanvas(Canvas_ID::RP2040_ID);
+    Canvas *canvas1 = matrix->CreateCanvas(Canvas_ID::RP2040_ID);
+
+    while (1) {
+        canvas0->Fill(255, 255, 255);
+        matrix->show(canvas0);
+        canvas1->Fill(0, 0, 0);
+        matrix->show(canvas1);
+    }
 
     return 0;
 }
