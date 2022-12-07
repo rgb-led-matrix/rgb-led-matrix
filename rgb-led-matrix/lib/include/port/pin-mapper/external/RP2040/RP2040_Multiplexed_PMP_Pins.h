5d80e27fbfc49aadaf411c61a6e17e2412e5da67
David Thacher <davidethacher@gmail.com>
2022-12-07 18:34:34 -0500

    Updates

--- rgb-led-matrix/lib/include/port/pin-mapper/external/RP2040/RP2040_Multiplexed_PMP_Pins.h
+++ rgb-led-matrix/lib/include/port/pin-mapper/external/RP2040/RP2040_Multiplexed_PMP_Pins.h
@@ -5,24 +5,23 @@
 
 namespace rgb_matrix {
 
-  struct cs_t {
-    cs_t(uint32_t cs0, uint32_t cs1, uint32_t cs2, uint32_t cs3) {
-      cs[0] = 1 << cs0;
-      cs[1] = 1 << cs1;
-      cs[2] = 1 << cs2;
-      cs[3] = 1 << cs3;
+  struct wr_t {
+    wr_t(uint32_t wr0, uint32_t wr1, uint32_t wr2, uint32_t wr3) {
+      wr[0] = 1 << wr0;
+      wr[1] = 1 << wr1;
+      wr[2] = 1 << wr2;
+      wr[3] = 1 << wr3;
     }
 
-    gpio_bits_t cs[4];
+    gpio_bits_t wr[4];
   };
 
   struct RP2040_Multiplexed_PMP_Pins : public PinMapping {
     public:
-      RP2040_Multiplexed_PMP_Pins(const char *name, uint32_t clk, uint32_t dat, cs_t cs, uint32_t reset) 
-        : PinMapping(name), clk(clk), dat(1 << dat), cs(cs), reset(1 << reset) {}
+      RP2040_Multiplexed_PMP_Pins(const char *name, uint32_t dat, wr_t wr, uint32_t reset) 
+        : PinMapping(name), dat(1 << dat), wr(wr), reset(1 << reset) {}
 
-      const cs_t cs;
-      const gpio_bits_t clk;
+      const wr_t wr;
       const uint32_t dat;       // Defines 8 pins (dat + 0, dat + 1, ..., dat + 7)
       const uint32_t reset;     // Not used
   };
