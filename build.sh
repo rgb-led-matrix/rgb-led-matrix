0b4e62524e9b9f5e3999c6ba12aa8a91c6451b33
David Thacher <davidethacher@gmail.com>
2022-12-07 10:48:48 -0500

    Updates

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cd external/RP2040/LED_Matrix_RP2040
+git submodule update --init
+groovy build.groovy -c cfg.xml
+./LED_Matrix/build/build.sh
+cd ../../../
+
 cd rgb-led-matrix
 mkdir build
 cd build
