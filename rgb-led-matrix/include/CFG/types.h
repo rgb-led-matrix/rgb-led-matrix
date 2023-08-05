5cb098131fec6a4eb40bd6cf6559f4354fc00a1b
David Thacher <davidethacher@gmail.com>
2023-08-05 18:44:11 -0400

    Updates

--- rgb-led-matrix/include/CFG/types.h
+++ rgb-led-matrix/include/CFG/types.h
@@ -17,11 +17,12 @@ namespace rgb_matrix {
     };
 
     struct GAMMA {
-        GAMMA(float red = 1.0, float green = 1.0, float blue = 1.0) : red_(red), green_(green), blue_(blue) {}
+        public:
+            GAMMA(float red = 1.0, float green = 1.0, float blue = 1.0) : red_(red), green_(green), blue_(blue) {}
 
-        inline float get_red() { return red_; }
-        inline float get_green() { return green_; }
-        inline float get_blue() { return blue_; }
+            inline float get_red() { return red_; }
+            inline float get_green() { return green_; }
+            inline float get_blue() { return blue_; }
 
         protected:
             float red_;
@@ -31,11 +32,11 @@ namespace rgb_matrix {
 
     struct DOTCorrect {
         public:
-        DOTCorrect(int rows, int cols);
-        ~DOTCorrect();
+            DOTCorrect(int rows, int cols);
+            ~DOTCorrect();
 
-        bool set(int x, int y, uint8_t r, uint8_t g, uint8_t b, float red, float green, float blue);
-        void get(int x, int y, uint8_t r, uint8_t g, uint8_t b, float *red, float *green, float *blue);
+            bool set(int x, int y, uint8_t r, uint8_t g, uint8_t b, float red, float green, float blue);
+            void get(int x, int y, uint8_t r, uint8_t g, uint8_t b, float *red, float *green, float *blue);
         
         private:
             bool check(float f);
