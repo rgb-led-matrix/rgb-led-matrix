4656cae44fc5b56a58899130b45dc091f450bb42
David Thacher <davidethacher@gmail.com>
2023-08-05 18:22:19 -0400

    Updates

--- rgb-led-matrix/lib/src/RGBMatrix.cpp
+++ rgb-led-matrix/lib/src/RGBMatrix.cpp
@@ -1,9 +1,15 @@
 #include "RGBMatrix.h"
+#include "CFG/CFG.h"
 #include "framebuffer/Framebuffer.h"
 #include "framebuffer/external/external.h"
 
 namespace rgb_matrix {
   Panel *RGBMatrix::CreatePanel() {
-    return Framebuffer<RGB48>::CreateFramebuffer(_cfg);
+    switch (_cfg->get_id()) {
+      case Panel_ID::RP2040_UART_RGB48_ID:
+        return Framebuffer<RGB48>::CreateFramebuffer(_cfg);
+      default:
+        return nullptr;
+    }
   }
 };
