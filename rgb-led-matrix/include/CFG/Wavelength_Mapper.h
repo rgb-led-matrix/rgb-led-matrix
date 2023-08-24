5f543fdd936810f7c97127e2b7caf37e6a67b8f1
David Thacher <davidethacher@gmail.com>
2023-08-24 17:19:46 -0400

    Updates

---
+++ rgb-led-matrix/include/CFG/Wavelength_Mapper.h
@@ -0,0 +1,12 @@
+#ifndef WAVELENGTH_MAPPER_H
+#define WAVELENGTH_MAPPER_H
+
+#include <stdint.h>
+
+namespace rgb_matrix {
+    template <typename T> class Wavelength_Mapper {
+
+    };
+}
+
+#endif
