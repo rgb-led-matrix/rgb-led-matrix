67e315a46b36b1872b79d339e6fbc338cc337685
David Thacher <davidethacher@gmail.com>
2023-09-14 08:14:56 -0400

    Updates

--- rgb-led-matrix/include/IO/Protocol/RP2040_UART/RP2040_UART.h
+++ rgb-led-matrix/include/IO/Protocol/RP2040_UART/RP2040_UART.h
@@ -2,9 +2,11 @@
 #define RP2040_UART_H
 
 #include <stdint.h>
+#include <IO/Protocol/Protocol.h>
+#include <IO/Node/FTDI_UART/FTDI_UART.h>
 
 namespace rgb_matrix {
-    class RP2040_UART {
+    class RP2040_UART : public Protocol, FTDI_UART {
         public:
             void send(uint8_t *buf, uint32_t size, uint8_t chunk);
     };
