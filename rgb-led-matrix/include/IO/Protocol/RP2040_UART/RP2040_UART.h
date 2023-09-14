0eb7a084a4d72571157c1bbbe7487e2896bad8d1
David Thacher <davidethacher@gmail.com>
2023-09-14 07:51:26 -0400

    Updates

--- rgb-led-matrix/include/IO/Protocol/RP2040_UART/RP2040_UART.h
+++ rgb-led-matrix/include/IO/Protocol/RP2040_UART/RP2040_UART.h
@@ -1,33 +1,12 @@
-    /*template <typename T> void RP2040_UART<T>::worker_thread(RP2040_UART<T> *object) {
-        uint32_t size = sizeof(T) * object->cfg_->get_cols() * object->cfg_->get_rows();
-        char *start = (char *) "s";
-        char *idle = (char *) "i";
+#ifndef RP2040_UART_H
+#define RP2040_UART_H
 
-        while (object->shutdown_) {
-            if (object->start_) {
-                // Send active tocken to get buss out of idle
-                object->cfg_->get_node()->write(start, 1);
+#include <stdint.h>
 
-                // Wait for bus to become active (timout after 100uS and drop frame)
-                for (int i = 0; i < 10; i++) {
-                    // If it times out then the bus is idle, send frame
-                    if (object->cfg_->get_node()->read(&idle, 1, 10) == 0) {
-                        // TODO: Send checksum of data using CRC-32/MPEG-2
-
-                        object->cfg_->get_node()->write((char *) object->buffer_, size);
-
-                        // Check for idle loop, recover if still active
-                        while (object->cfg_->get_node()->read(&idle, 1, 100) == 0)
-                            object->cfg_->get_node()->write(idle, 1);
-                    
-                        break;
-                    }
-                }
-
-                object->start_ = false;
-            }
-
-            // Sink idle tokens
-            object->cfg_->get_node()->read(&idle, 1, 10);
-        }
-    }*/
+namespace rgb_matrix {
+  class RP2040_UART {
+    public:
+      void send(uint8_t *buf, uint32_t size, uint8_t chunk);
+  };
+}
+#endif
