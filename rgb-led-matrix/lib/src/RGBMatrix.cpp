ee9081341671cb4650108e5a08119e39947e8ece
David Thacher <davidethacher@gmail.com>
2024-02-16 08:19:20 -0500

    updates

--- rgb-led-matrix/lib/src/RGBMatrix.cpp
+++ rgb-led-matrix/lib/src/RGBMatrix.cpp
@@ -8,19 +8,19 @@
 #include <Framebuffer/RGB/RGB_222.h>
 
 namespace rgb_matrix {
-  Panel *RGBMatrix::CreatePanel(CFG *cfg) {
-    switch (cfg->get_data_format()) {
-      case Data_Format_ID::RGB48_ID:
-        return new Framebuffer<RGB48>(cfg);
-      case Data_Format_ID::RGB24_ID:
-        return new Framebuffer<RGB24>(cfg);
-      case Data_Format_ID::RGB_222_ID:
-        return new Framebuffer<RGB_222>(cfg);
-      case Data_Format_ID::RGB_555_ID:
-        return new Framebuffer<RGB_555>(cfg);
-      default:
-        throw Unknown_Type("Data_Format_ID");
-        break;
+    Panel *RGBMatrix::CreatePanel(CFG *cfg) {
+        switch (cfg->get_data_format()) {
+            case Data_Format_ID::RGB48_ID:
+                return new Framebuffer<RGB48>(cfg);
+            case Data_Format_ID::RGB24_ID:
+                return new Framebuffer<RGB24>(cfg);
+            case Data_Format_ID::RGB_222_ID:
+                return new Framebuffer<RGB_222>(cfg);
+            case Data_Format_ID::RGB_555_ID:
+                return new Framebuffer<RGB_555>(cfg);
+            default:
+                throw Unknown_Type("Data_Format_ID");
+                break;
+        }
     }
-  }
 };
