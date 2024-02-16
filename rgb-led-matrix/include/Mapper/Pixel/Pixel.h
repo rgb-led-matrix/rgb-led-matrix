0787b59c603391cbd2242c47f398b1dc15887072
David Thacher <davidethacher@gmail.com>
2024-02-16 15:15:09 -0500

    I have wasted so much time on this. Just call show from the Pixel implementation. I am protected by the constructor for show. I fixed MultiPanel in theory...

--- rgb-led-matrix/include/Mapper/Pixel/Pixel.h
+++ rgb-led-matrix/include/Mapper/Pixel/Pixel.h
@@ -6,18 +6,23 @@
 
 namespace rgb_matrix {
     // Optional construct for handling pixel locations and RGB ordering
-    class Pixel {
+    class Pixel : public Single_Panel {
         public:
+            Pixel(Single_Panel *panel);
+            virtual ~Pixel();
+
             void SetPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue);
 
         protected:
+            Pixel();
+
             virtual cord_t map_location(int x, int y) = 0;
             virtual Color_Order map_color(int x, int y) = 0;
 
             Single_Panel *panel_;
 
         private:
-            virtual void map() = 0;
+            void map();
 
             cord_t **locations_;
             Color_Order **orders_;
