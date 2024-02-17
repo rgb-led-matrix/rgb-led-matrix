82f6e2d6d479e5e54f2930395f809fda22e1ee75
David Thacher <davidethacher@gmail.com>
2024-02-17 08:48:10 -0500

    Remove Panel interface

--- rgb-led-matrix/include/Panel/MultiPanel.h
+++ rgb-led-matrix/include/Panel/MultiPanel.h
@@ -7,10 +7,12 @@
 namespace rgb_matrix {
     // Pure interface for creating large panel from multiple panels
     //  Can be used to create weird setups
-    class MultiPanel : public Panel {
+    class MultiPanel : public Mapper {
         public:
             virtual bool map_panel(int x, int y, Single_Panel *panel, Protocol *protocol) = 0;
             virtual void show() = 0;
+            virtual void set_brightness(uint8_t brightness) = 0;
+            virtual void map_wavelength(uint8_t color, Color index, uint16_t value) = 0;
     };
 }
 #endif
--- rgb-led-matrix/include/Panel/Single_Panel.h
+++ rgb-led-matrix/include/Panel/Single_Panel.h
@@ -1,13 +1,16 @@
 #ifndef SINGLE_PANEL_H
 #define SINGLE_PANEL_H
 
-#include <Panel/Panel.h>
+#include <Mapper/Mapper.h>
+#include <IO/Protocol/Protocol.h>
 
 namespace rgb_matrix {
     // Pure interface for Single Panel
-    class Single_Panel : public Panel {
+    class Single_Panel : public Mapper {
         public:
             virtual void show(Protocol *protocol) = 0;
+            virtual void set_brightness(uint8_t brightness) = 0;
+            virtual void map_wavelength(uint8_t color, Color index, uint16_t value) = 0;
     };
 }
 #endif
