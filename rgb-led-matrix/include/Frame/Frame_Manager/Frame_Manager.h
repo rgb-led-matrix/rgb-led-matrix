e59b5959e57c335f8c2cbfe7c32f67a59b71d058
David Thacher <davidethacher@gmail.com>
2024-02-17 19:45:31 -0500

    Updates

--- rgb-led-matrix/include/Frame/Frame_Manager/Frame_Manager.h
+++ rgb-led-matrix/include/Frame/Frame_Manager/Frame_Manager.h
@@ -2,8 +2,12 @@
 #define FRAME_MANAGER_H
 
 #include <queue>
+#include <thread>
+#include <mutex>
 #include <stdint.h>
 #include <Frame/Frame.h>
+using std::thread;
+using std::mutex;
 
 namespace rgb_matrix {
     // Optional construct for managing framerate
@@ -15,7 +19,14 @@ namespace rgb_matrix {
             void push_frame(Frame *frame);
 
         protected:
+            static void worker_thread(Frame_Manager *object);
+
             std::queue<Frame *> frames_;
+            thread *thread_;
+            mutex lock_;
+            int framerate_;
+            bool shutdown_;
+            bool isAsync_;
     };
 }
 #endif
--- rgb-led-matrix/lib/src/Frame/Frame_Manager/Frame_Manager.cpp
+++ rgb-led-matrix/lib/src/Frame/Frame_Manager/Frame_Manager.cpp
@@ -1,21 +1,68 @@
+#include <chrono>
 #include <Frame/Frame_Manager/Frame_Manager.h>
 #include <Exception/Null_Pointer.h>
+#include <Exception/Illegal.h>
 
 namespace rgb_matrix {
     Frame_Manager::Frame_Manager(int framerate, bool isAsync) {
         String_Exception("Not finished");
 
+        if (framerate <= 0 || framerate >= 1000)
+            throw Illegal("Framerate");
+
+        isAsync_ = isAsync;
+        framerate_ = framerate;
+        shutdown_ = false;
+        thread_ = new std::thread(&Frame_Manager::worker_thread, this);
+
         // TODO:
     }
 
     Frame_Manager::~Frame_Manager() {
-        // TODO:
+        shutdown_ = true;
+        thread_->join();
+        delete thread_;
     }
 
     void Frame_Manager::push_frame(Frame *frame) {
         if (frame == nullptr)
             throw Null_Pointer("Frame");
 
-        // TODO:
+        lock_.lock();
+        frame->isFree_ = false;
+        frames_.push(frame);
+        lock_.unlock();
+    }
+
+    void Frame_Manager::worker_thread(Frame_Manager *object) {
+        uint64_t time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        uint64_t range = 1000 / object->framerate_;
+
+        while (!object->shutdown_) {
+            if (object->isAsync_) {
+                if ((std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count() - time) > range) {
+                    if (!object->frames_.empty())
+                        object->frames_.pop();
+
+                    time += 1000 / object->framerate_;
+                }
+
+                time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+            }
+
+            object->lock_.lock();
+            Frame *f = object->frames_.front();
+            object->lock_.unlock();
+            
+            if (f->isMulti_)
+                f->multi_->show();
+            else
+                f->single_->show(f->protocol_);
+
+            object->lock_.lock();
+            f->isFree_ = true;
+            object->frames_.pop();
+            object->lock_.unlock();
+        }
     }
 }
