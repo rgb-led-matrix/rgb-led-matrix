05aed2293c2e152f247104510a26ac6d79b8afbc
David Thacher <davidethacher@gmail.com>
2024-02-21 10:49:49 -0500

    Updates

---
+++ rgb-led-matrix/include/Mapper/Outdoor/Outdoor.h
@@ -0,0 +1,13 @@
+#ifndef OUTDOOR_H
+#define OUTDOOR_H
+
+#include <Mapper/Mapper.h>
+
+namespace rgb_matrix {
+    class Outdoor : public Mapper {
+        public:
+            Color_Order map_color(uint16_t x, uint16_t y);
+            cord_t map_location(uint16_t x, uint16_t y, cord_t size, uint8_t scan);
+    };
+}
+#endif
