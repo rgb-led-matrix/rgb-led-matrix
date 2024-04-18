62aed606b068cfa5c3172819ff030461e04a06a3
David Thacher <davidethacher@gmail.com>
2024-04-18 19:02:44 -0400

    updates

--- rgb-led-matrix/include/IO/Control/COM_Control/COM_Control.h
+++ rgb-led-matrix/include/IO/Control/COM_Control/COM_Control.h
@@ -17,6 +17,20 @@ namespace rgb_matrix {
 
         protected:
             COM_Control();
+
+            struct Control_Message {
+                Control_Message(Commands command);
+
+                uint32_t header;
+                uint8_t cmd;
+                uint16_t len;
+                uint8_t id;
+                uint32_t checksum;
+                uint32_t delimiter;
+
+                private:
+                    uint32_t compute_checksum();
+            };
     };
 }
 #endif
--- rgb-led-matrix/lib/src/IO/Control/COM_Control/COM_Control.cpp
+++ rgb-led-matrix/lib/src/IO/Control/COM_Control/COM_Control.cpp
@@ -1,4 +1,5 @@
 #include <IO/Control/COM_Control/COM_Control.h>
+#include <IO/CRC/CRC.h>
 #include <Exception/Illegal.h>
 #include <Exception/Unknown_Type.h>
 
@@ -14,13 +15,46 @@ namespace rgb_matrix {
     }
 
     void COM_Control::signal(Commands command) {
-        // TODO:
+        Control_Message *msg = new Control_Message(command);
+        // TODO: Send it
+    }
+
+    COM_Control::Control_Message::Control_Message(Commands command) {
+        header = htonl(0xAAEEAAEE);
+        len = 1;
+        id = 0;
+        delimiter = htonl(0xAEAEAEAE);
+
         switch (command) {
             case Commands::Trigger:
+                cmd = 0;
+                break;
             case Commands::Reset:
+                cmd = 1;
+                break;
             default:
                 throw Unknown_Type("Commands");
                 break;
         }
+
+        checksum = htonl(compute_checksum());
+    }
+
+    static inline uint32_t checksum_chunk(uint32_t checksum, uint32_t v, uint8_t bits) {
+        for (int i = 0; i < bits; i += 8)
+            checksum = rgb_matrix::CRC::crc32(checksum, (v >> i) & 0xFF);
+        
+        return checksum;
+    }
+
+    inline uint32_t COM_Control::Control_Message::compute_checksum() {
+        uint32_t checksum = 0xFFFFFFFF;
+
+        checksum = checksum_chunk(checksum, header, 32);
+        checksum = checksum_chunk(checksum, cmd, 8);
+        checksum = checksum_chunk(checksum, len, 16);
+        checksum = checksum_chunk(checksum, status, 32);
+
+        return ~checksum;
     }
 }
