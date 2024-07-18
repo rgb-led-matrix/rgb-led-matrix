926b30e5df06b00867f0a69c67473b13e572b78a
David Thacher <davidethacher@gmail.com>
2024-07-18 17:15:05 -0400

    This should have been in the first draft

--- rgb-led-matrix/lib/include/Panel/SIMD/SIMD_SINGLE.h
+++ rgb-led-matrix/lib/include/Panel/SIMD/SIMD_SINGLE.h
@@ -2,12 +2,19 @@
 #define SIMD_H
 
 namespace rgb_matrix::SIMD {
-    template <typename T> class SIMD_SINGLE {
+    template <typename T> class SIMD_SINGLE {   // Single is 128-bit SIMD
         public:
             SIMD_SINGLE<T> operator*(SIMD_SINGLE<T> const& arg);
             SIMD_SINGLE<T> operator/(SIMD_SINGLE<T> const& arg);
 
-            T v[4];
+            union {
+                T v[4];
+                uint8_t  b[16];
+                uint16_t s[8];
+                uint32_t l[4];
+                uint64_t ll[2];
+                //uint128_t lll;  // Compiler does not support?
+            };
     };
 
     template<typename T, typename R> void round(SIMD_SINGLE<T> arg, SIMD_SINGLE<R> *result);
